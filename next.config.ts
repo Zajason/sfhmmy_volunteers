66aea3a60a6f9f36be0935f3980254d2cb4900f3
Stefanos Fragoulis <stefanos.fragoulis@outlook.com.gr>
2025-04-22 20:12:24 +0300

    Build

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // …your other config
 
-  // EXACT origins (protocol + host + port)
-  allowedDevOrigins: [
-    "http://localhost:3000",
-    "http://192.168.1.107:3000",
-  ],
+  
 };
 
 export default nextConfig;
